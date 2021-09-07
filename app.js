1a8728c95a2e9ac0ce0ccb75fb9971cdf3f56571
radibarq <grayllow@gmail.com>
2021-09-07 21:05:24 +0300

    setup npm, add request pacakge and test simple request to the weather api

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
+const request = require('request')
 
+const url = "http://api.weatherstack.com/current?access_key=84dc24a6387f03b2319ace30f96658a8&query=37.8267,-122.4233"
 
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
