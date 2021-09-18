434edcc580eda915acd72f309126d03c5ded78e2
radibaraq <radibaraq@cloud.upwork.com>
2021-09-19 01:07:22 +0300

    Geocoding demonstration

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ request({ url: url, json: true }, (error, response) => {
     console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. But it feels like ' + response.body.current.feelslike + ' degrees out')
 })
 
-
-let geoCodingURL = "http://api.weatherstack.com/current?access_key=84dc24a6387f03b2319ace30f96658a8&query=37.8267,-122.4233&units=f"
-request({url: geoCodingURL, json: true}, (error, response) => {
-    let coordinates = response.body.features[0]
-    console.log("lat is " + coordinates[0] + "long is " + coordinates[1])
+let geocodingURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmFkaWJhcmFxIiwiYSI6ImNrdHFiNzF0ZzB1eGMybm1wdm01dmQ5dmsifQ.Yla452h33DKewn4M-69T0A"
+request({url: geocodingURL, json: true}, (error, response) => {
+    let coordinates = response.body.features[0].geometry.coordinates
+    console.log("lat is " + coordinates[0] + " and long is " + coordinates[1])
 })
 
 // Geocoding
